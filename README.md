c09a281762de1ca1c18de4147a564d7093730ea8
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-12-09 16:46:57 +0100

    ejercicio 4

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# daw-examen
+# dwes-examen
+## Javi Barrientos
+
+4- Modificar para mostrar dolar, listado, read_one
+`index.php line 91 $ before {$price}`
---
+++ php-beginner-crud-level-1/config/database.php
@@ -0,0 +1,15 @@
+<?php
+// used to connect to the database
+$host = "localhost";
+$db_name = "php_beginner_crud_level_1";
+$username = "root";
+$password = "Q!w2e3r4t5";
+
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+}
+
+// show error
+ catch (PDOException $exception) {
+    echo "Connection error: " . $exception->getMessage();
+}
